b65b14ee53fc6cbe895f5b2eb98ddf18572c9385
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2013-11-26 01:38:58 +0200

    Security Issue fixed

--- SusiParser/StudentRole.cs
+++ SusiParser/StudentRole.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace SusiParser
 {
+	[DataContract]
 	public struct StudentRole
 	{
+		[DataMember(Name="roleText")]
 		public string RoleText { get; private set; }
+		[DataMember(Name="facultyNumber")]
 		public string FacultyNumber { get; private set; }
+
+		[IgnoreDataMember]
 		public int HiddenUserId { get; private set; }
+		[IgnoreDataMember]
 		public int HiddenRoleId { get; private set; }
 
 		public StudentRole(string roleText, string facultyNumber, int userId, int roleId) : 
